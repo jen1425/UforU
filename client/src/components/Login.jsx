08d0ea17517ac35255240ace46cf63f5f6227795
Aaron Xavier <aarondxavier@gmail.com>
2017-05-19 16:02:15 -0700

    Updated content and styling on Login and Signup

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -43,7 +43,18 @@ class Login extends React.Component {
     }
 
     return (
-      <div className = "signupContain">
+       <div className="container-fluid-fullwidth">
+        <div className="container" id="banner">
+          <h1>
+            <b>UFORU</b>
+          </h1>
+          <hr></hr>
+          <h4>
+            UNIVERSITY FOR YOU
+          </h4>
+          <hr></hr>
+        </div>
+        <div className = "signupContain">
         <p className = "loginText"> Please Login below </p>
         <div className="row">
           <div className="col-md-2 col-md-offset-5">
@@ -65,6 +76,35 @@ class Login extends React.Component {
           {
             this.state.showError ? <h6 className="text-center"><small>Username or password is incorrect.</small></h6> : <div></div>
           }
+        </div>
+      </div>
+        <div className="card">
+          <div className="row" className="bio">
+            <h6><b><u>ABOUT THE CREATORS</u></b></h6>
+            <div className="col-md-4">
+              <img src="images/farrah_bousetta.png" className="img-responsive bioImages" style={{ height: 200, width: 200 }} alt="FARRAH PHOTO HERE"/>
+            </div>
+            <div className="col-md-4">
+              <img src="images/arseniy_kotov.png" className="img-responsive bioImages" style={{ height: 200, width: 200}} alt="ARSENIY PHOTO HERE"/>
+            </div>
+            <div className="col-md-4">
+              <img src="images/helen_tang.png" className="img-responsive bioImages" style={{ height: 200, width: 200 }} alt="HELEN PHOTO HERE"/>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-4">
+              <p>Farrah Bousetta is an upcoming professional software engineer with previous experience at Facebook, Google and other prestegious tech companies. She gets stuff done. Her nickname is Feisty Farrah.</p>
+            </div>
+            <div className="col-md-4">
+              <p>Arseniy Kotov is an all star programmer specializing in full stack developement.</p>
+            </div>
+            <div className="col-md-4">
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            </div>
+          </div>
+         </div>
+        <div className = "container-fluid-fullwidth">
+          <div className="navbar-default navbar-fixed-bottom">Made by Farrah Bousetta, Arseniy Kotov, and Helen Tang</div>
         </div>
       </div>
     );
--- client/src/components/Signup.jsx
+++ client/src/components/Signup.jsx
@@ -53,6 +53,17 @@ class Signup extends React.Component {
       return <Redirect to={{pathname: '/home', state: {isLoggedIn: this.state.isLoggedIn}}} />;
     }
     return (
+       <div className="container-fluid-fullwidth">
+        <div className="container" id="banner">
+          <h1>
+            <b>UFORU</b>
+          </h1>
+          <hr></hr>
+          <h4>
+            UNIVERSITY FOR YOU
+          </h4>
+          <hr></hr>
+        </div>
       <div className = "signupContain">
         <p className = "loginText"> Create an account </p>
         <div className="row">
@@ -72,6 +83,35 @@ class Signup extends React.Component {
           {
             this.state.showError ? <h6 className="text-center"><small>Username is already taken.</small></h6> : <div></div>
           }
+        </div>
+      </div>
+        <div className="card">
+          <div className="row" className="bio">
+            <h6><b><u>ABOUT THE CREATORS</u></b></h6>
+            <div className="col-md-4">
+              <img src="images/farrah_bousetta.png" className="img-responsive bioImages" style={{ height: 200, width: 200 }} alt="FARRAH PHOTO HERE"/>
+            </div>
+            <div className="col-md-4">
+              <img src="images/arseniy_kotov.png" className="img-responsive bioImages" style={{ height: 200, width: 200}} alt="ARSENIY PHOTO HERE"/>
+            </div>
+            <div className="col-md-4">
+              <img src="images/helen_tang.png" className="img-responsive bioImages" style={{ height: 200, width: 200 }} alt="HELEN PHOTO HERE"/>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-4">
+              <p>Farrah Bousetta is an upcoming professional software engineer with previous experience at Facebook, Google and other prestegious tech companies. She gets stuff done. Her nickname is Feisty Farrah.</p>
+            </div>
+            <div className="col-md-4">
+              <p>Arseniy Kotov is an all star programmer specializing in full stack developement.</p>
+            </div>
+            <div className="col-md-4">
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+            </div>
+          </div>
+         </div>
+        <div className = "container-fluid-fullwidth">
+          <div className="navbar-default navbar-fixed-bottom">Made by Farrah Bousetta, Arseniy Kotov, and Helen Tang</div>
         </div>
       </div>
     );
